c7ef62eab79a9d46662d0ba8ee076ee4ea4f11f7
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-08-18 10:00:55 -0300

    First commit

---
+++ src/main/java/com/tiagopalte/habanero/HabaneroApplication.java
@@ -0,0 +1,12 @@
+package com.tiagopalte.habanero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabaneroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabaneroApplication.class, args);
+	}
+}
